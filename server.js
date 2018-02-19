bd69a2cdb664c87ee4a85ab7a37594ba22ee1169
gamesmartz <awnold@hotmail.com>
2018-02-19 13:30:36 -0800

    node working port 9000

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const PORT = process.env.PORT || 9000;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('<h1>Working</h1>');
+});
+
+app.listen(PORT, () => {
+    console.log('server running on PORT: '+  PORT);
+});
