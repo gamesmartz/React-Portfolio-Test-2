5c9c05843339542b956baf91b12cf33e81c95c95
gamesmartz <awnold@hotmail.com>
2018-02-19 17:51:35 -0800

    bundled and working with server

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const cors = require('cors');
+const path =  require('path');
 const PORT = process.env.PORT || 9000;
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('<h1>Working!</h1>');
+app.use(cors());
+
+app.use(express.static(path.resolve(__dirname, 'client', 'dist')));
+
+// app.get('/', (req, res) => {
+//     res.send('<h1>Working!</h1>');
+// });
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
 });
 
 
